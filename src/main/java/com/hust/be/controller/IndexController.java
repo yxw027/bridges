4538137a0c4995bf79b1c22aa5ff077a3a322517
mzyan <mengzhao.yan@foxmail.com>
2020-07-18 23:20:32 +0800

    Contents内容下的三个主页面的跳转控制

--- src/main/java/com/hust/be/controller/IndexController.java
+++ src/main/java/com/hust/be/controller/IndexController.java
@@ -1,10 +1,29 @@
 package com.hust.be.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Created by mzYan on 2020-07-18 15:28
  */
 @Controller
 public class IndexController {
+
+    @GetMapping("/monitor")
+    public String toMonitor(){
+        System.out.println("page switch to monitor.");
+        return "index";
+    }
+
+    @GetMapping("/calendar")
+    public String toCalendar(){
+        System.out.println("page switch to calendar.");
+        return "calendar";
+    }
+
+    @GetMapping("/info")
+    public String toInfo(){
+        System.out.println("page switch to info.");
+        return "info";
+    }
 }
